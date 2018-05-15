03d9c0cde5b097748875d35fe98411332f8f9d25
Robin Gögge <robin.goegge@webtrekk.com>
2018-05-15 17:25:06 +0200

    simplify two-sum.go -> better readability

--- two-sum.go
+++ two-sum.go
@@ -1,15 +1,16 @@
 package main
 
 func twoSum(nums []int, target int) []int {
+	var res []int
 	lookup := make(map[int]int)
 
 	for i, num := range nums {
 		index := target - num
 		if _, ok := lookup[index]; ok {
-			res := [2]int{lookup[index], i}
-			return res[:2]
+			res = []int{lookup[index], i}
+			return res
 		}
 		lookup[num] = i
 	}
-	return make([]int, 2)
+	return nil
 }
